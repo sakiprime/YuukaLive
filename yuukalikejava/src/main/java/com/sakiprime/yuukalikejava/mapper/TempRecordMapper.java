a84d3f6a3ac525d9c6bff71f3b7b1f2bcf1c9226
郑锦荣 <2456032877@qq.com>
2026-07-05 14:30:22 +0800

    Delete yuukalikejava/src/main directory

--- yuukalikejava/src/main/java/com/sakiprime/yuukalikejava/mapper/TempRecordMapper.java
+++
@@ -1,9 +0,0 @@
-package com.sakiprime.yuukalikejava.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.sakiprime.yuukalikejava.entity.TempRecordEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TempRecordMapper extends BaseMapper<TempRecordEntity> {
-}
